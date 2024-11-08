cd65fa287708f080601a3e4954796b182805a8f1
Daniel Escribano <daniel.escribanoissacoivtch@usp.ceu.es>
2024-11-08 16:49:33 +0100

    Arreglado html de pagar, carro, obtener producto y progreso

--- src/public/JS/pagar.js
+++ src/public/JS/pagar.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const payButton = document.getElementById('pay-button');
 
     payButton.addEventListener('click', function () {
-        window.location.href = 'obtenerproducto.html';
+        window.location.href = 'obtener_producto.html';
     });
 });
