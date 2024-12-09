b5fd1f5f4c5835cc5756fc4df0657931a9552cca
alvaro-sanzc <76051058+alvaro-sanzc@users.noreply.github.com>
2024-12-09 16:54:10 +0100

    Guia Ejercicios Arreglada

--- src/public/JS/guia_ejercicios.js
+++ src/public/JS/guia_ejercicios.js
@@ -8,6 +8,8 @@ async function cargarEjercicio() {
         if (!nombreEjercicio) {
             throw new Error('No se proporcionó un nombre de ejercicio en la URL.');
         }
+        
+        cargarImagenUsuario()
 
         // Realizamos una solicitud POST al servidor para obtener los datos del ejercicio
         const responseEjercicio = await fetch('/api/guia-ejercicios', {
@@ -46,15 +48,10 @@ async function cargarEjercicio() {
             .map(step => `<li>${step.trim()}.</li>`)
             .join('');
 
-        // Solicitar la imagen si está disponible
-        if (ejercicio.imagen) {
-            cargarImagenUsuario()
-        }
 
     } catch (error) {
         console.error('Error al cargar los datos del ejercicio:', error);
     }
-    cargarImagenUsuario()
 }
 
 
@@ -89,6 +86,7 @@ async function cargarImagenUsuario() {
 
         const imgElemento = document.getElementById('imagen-ejercicio');
         imgElemento.src = datos.imagen; // datos.imagen es el Base64 devuelto por la API
+        console.log(datos.imagen)
     } catch (error) {
         console.error('Error al cargar la imagen:', error);
     }
